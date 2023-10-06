e8405a5e78d927a5623a3cee4c716cb525fa29c5
johanaarstein <johan@aarstein.media>
2023-10-06 15:17:37 +0200

    typesfux

--- types/component/functions.d.ts
+++ types/component/functions.d.ts
@@ -1,4 +1,16 @@
-import type { ObjectFit } from './types';
-declare const aspectRatio: (objectFit: ObjectFit) => "none" | "xMidYMid meet" | "xMidYMid slice" | "xMinYMin slice", fetchPath: (path: string) => Promise<JSON>;
-export { aspectRatio, fetchPath };
-//# sourceMappingURL=functions.d.ts.map
+import type { Unzipped } from 'fflate';
+import type { LottieAsset, LottieJSON, LottieManifest, ObjectFit } from './types';
+export declare const aspectRatio: (objectFit: ObjectFit) => "none" | "xMidYMid meet" | "xMidYMid slice" | "xMinYMin slice", download: (data: string | ArrayBuffer, options?: {
+    name: string;
+    mimeType: string;
+}) => void, handleErrors: (err: unknown) => {
+    message: string;
+    status: number;
+}, frameOutput: (frame?: number) => string, getAnimationData: (input: unknown) => Promise<{
+    animations: LottieJSON[] | null;
+    manifest: LottieManifest | null;
+    isDotLottie?: boolean;
+}>, getExt: (str: string) => string, getFilename: (src: string, keepExt?: boolean) => string, getLottieJSON: (resp: Response) => Promise<{
+    data: LottieJSON[];
+    manifest: LottieManifest;
+}>, getManifest: (unzipped: Unzipped) => LottieManifest, getMimeFromExt: (ext?: string) => string, isAudio: (asset: LottieAsset) => boolean, isImage: (asset: LottieAsset) => boolean, isServer: () => boolean, resolveAssets: (unzipped: Unzipped, assets?: LottieAsset[]) => Promise<void>, unzip: (resp: Response) => Promise<Unzipped>, useId: (prefix?: string) => string;
