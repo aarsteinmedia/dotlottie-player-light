40f10feca07af12ce0bd0244ef5b18f1ea7e4844
Johan Martin Aarstein <johan@aarstein.media>
2025-03-07 21:03:53 +0100

    renderers oop

--- src/renderers/SVGElementsRenderer.ts
+++ src/renderers/SVGElementsRenderer.ts
@@ -312,3 +312,285 @@ const SVGElementsRenderer = (() => {
 })()
 
 export default SVGElementsRenderer
+
+// export default class SVGElementsRenderer {
+//   private static _identityMatrix = new Matrix()
+//   private static _matrixHelper = new Matrix()
+
+//   static createRenderFunction(data: Shape) {
+//     switch (data.ty) {
+//       case ShapeType.Fill:
+//         return this.renderFill
+//       case ShapeType.GradientFill:
+//         return this.renderGradient
+//       case ShapeType.GradientStroke:
+//         return this.renderGradientStroke
+//       case ShapeType.Stroke:
+//         return this.renderStroke
+//       case ShapeType.Path:
+//       case ShapeType.Ellipse:
+//       case ShapeType.Rectangle:
+//       case ShapeType.PolygonStar:
+//         return this.renderPath
+//       case ShapeType.Transform:
+//         return this.renderContentTransform
+//       case ShapeType.NoStyle:
+//         return this.renderNoop
+//       default:
+//         return null
+//     }
+//   }
+
+//   private static renderContentTransform(
+//     _: any,
+//     itemData: any,
+//     isFirstFrame: boolean
+//   ) {
+//     if (isFirstFrame || itemData.transform.op._mdf) {
+//       itemData.transform.container.setAttribute(
+//         'opacity',
+//         itemData.transform.op.v
+//       )
+//     }
+//     if (isFirstFrame || itemData.transform.mProps._mdf) {
+//       itemData.transform.container.setAttribute(
+//         'transform',
+//         itemData.transform.mProps.v.to2dCSS()
+//       )
+//     }
+//   }
+
+//   private static renderFill(
+//     _: StyleData,
+//     itemData: ItemData,
+//     isFirstFrame: boolean
+//   ) {
+//     const styleElem = itemData.style
+
+//     if (itemData.c._mdf || isFirstFrame) {
+//       styleElem.pElem.setAttribute(
+//         'fill',
+//         `rgb(${Math.floor((itemData.c.v as number[])[0])},${Math.floor(
+//           (itemData.c.v as number[])[1]
+//         )},${(itemData.c.v as number[])[2]})`
+//       )
+//     }
+//     if (itemData.o._mdf || isFirstFrame) {
+//       styleElem.pElem.setAttribute('fill-opacity', itemData.o.v as string)
+//     }
+//   }
+
+//   private static renderGradient(
+//     styleData: StyleData,
+//     itemData: any,
+//     isFirstFrame: boolean
+//   ) {
+//     const gfill = itemData.gf
+//     const hasOpacity = itemData.g._hasOpacity
+//     const pt1 = itemData.s.v
+//     const pt2 = itemData.e.v
+
+//     if (itemData.o._mdf || isFirstFrame) {
+//       const attr = styleData.ty === 'gf' ? 'fill-opacity' : 'stroke-opacity'
+//       itemData.style.pElem.setAttribute(attr, itemData.o.v)
+//     }
+//     if (itemData.s._mdf || isFirstFrame) {
+//       const attr1 = styleData.t === 1 ? 'x1' : 'cx'
+//       const attr2 = attr1 === 'x1' ? 'y1' : 'cy'
+//       gfill.setAttribute(attr1, pt1[0])
+//       gfill.setAttribute(attr2, pt1[1])
+//       if (hasOpacity && !itemData.g._collapsable) {
+//         itemData.of.setAttribute(attr1, pt1[0])
+//         itemData.of.setAttribute(attr2, pt1[1])
+//       }
+//     }
+//     let stops
+//     let i
+//     let len
+//     let stop
+//     if (itemData.g._cmdf || isFirstFrame) {
+//       stops = itemData.cst
+//       const cValues = itemData.g.c
+//       len = stops.length
+//       for (i = 0; i < len; i += 1) {
+//         stop = stops[i]
+//         stop.setAttribute('offset', `${cValues[i * 4]}%`)
+//         stop.setAttribute(
+//           'stop-color',
+//           `rgb(${cValues[i * 4 + 1]},${cValues[i * 4 + 2]},${cValues[i * 4 + 3]
+//           })`
+//         )
+//       }
+//     }
+//     if (hasOpacity && (itemData.g._omdf || isFirstFrame)) {
+//       const oValues = itemData.g.o
+//       if (itemData.g._collapsable) {
+//         stops = itemData.cst
+//       } else {
+//         stops = itemData.ost
+//       }
+//       len = stops.length
+//       for (i = 0; i < len; i += 1) {
+//         stop = stops[i]
+//         if (!itemData.g._collapsable) {
+//           stop.setAttribute('offset', `${oValues[i * 2]}%`)
+//         }
+//         stop.setAttribute('stop-opacity', oValues[i * 2 + 1])
+//       }
+//     }
+//     if (styleData.t === 1) {
+//       if (itemData.e._mdf || isFirstFrame) {
+//         gfill.setAttribute('x2', pt2[0])
+//         gfill.setAttribute('y2', pt2[1])
+//         if (hasOpacity && !itemData.g._collapsable) {
+//           itemData.of.setAttribute('x2', pt2[0])
+//           itemData.of.setAttribute('y2', pt2[1])
+//         }
+//       }
+//     } else {
+//       let rad
+//       if (itemData.s._mdf || itemData.e._mdf || isFirstFrame) {
+//         rad = Math.sqrt(
+//           Math.pow(pt1[0] - pt2[0], 2) + Math.pow(pt1[1] - pt2[1], 2)
+//         )
+//         gfill.setAttribute('r', rad)
+//         if (hasOpacity && !itemData.g._collapsable) {
+//           itemData.of.setAttribute('r', rad)
+//         }
+//       }
+//       if (
+//         itemData.e._mdf ||
+//         itemData.h._mdf ||
+//         itemData.a._mdf ||
+//         isFirstFrame
+//       ) {
+//         if (!rad) {
+//           rad = Math.sqrt(
+//             Math.pow(pt1[0] - pt2[0], 2) + Math.pow(pt1[1] - pt2[1], 2)
+//           )
+//         }
+//         const ang = Math.atan2(pt2[1] - pt1[1], pt2[0] - pt1[0])
+
+//         let percent = itemData.h.v
+//         if (percent >= 1) {
+//           percent = 0.99
+//         } else if (percent <= -1) {
+//           percent = -0.99
+//         }
+//         const dist = rad * percent
+//         const x = Math.cos(ang + itemData.a.v) * dist + pt1[0]
+//         const y = Math.sin(ang + itemData.a.v) * dist + pt1[1]
+//         gfill.setAttribute('fx', x)
+//         gfill.setAttribute('fy', y)
+//         if (hasOpacity && !itemData.g._collapsable) {
+//           itemData.of.setAttribute('fx', x)
+//           itemData.of.setAttribute('fy', y)
+//         }
+//       }
+//       // gfill.setAttribute('fy','200');
+//     }
+//   }
+
+//   private static renderGradientStroke(
+//     styleData: StyleData,
+//     itemData: ItemData,
+//     isFirstFrame: boolean
+//   ) {
+//     this.renderGradient(styleData, itemData, isFirstFrame)
+//     this.renderStroke(styleData, itemData, isFirstFrame)
+//   }
+
+//   private static renderNoop() { }
+
+//   private static renderPath(
+//     styleData: StyleData,
+//     itemData: ShapeHandler,
+//     isFirstFrame: boolean
+//   ) {
+//     let j: number
+//     let jLen: number
+//     let pathStringTransformed
+//     let redraw
+//     let pathNodes
+//     let l: number
+//     const lLen = itemData.styles.length
+//     const lvl = itemData.lvl
+//     let paths: ShapeDataProperty['paths']
+//     let mat
+//     let iterations
+//     let k
+//     for (l = 0; l < lLen; l += 1) {
+//       redraw = itemData.sh?._mdf || isFirstFrame
+//       if (itemData.styles[l].lvl < lvl) {
+//         mat = this._matrixHelper.reset()
+//         iterations = lvl - itemData.styles[l].lvl
+//         k = itemData.transformers.length - 1
+//         while (!redraw && iterations > 0) {
+//           redraw = itemData.transformers[k].mProps._mdf || redraw
+//           iterations -= 1
+//           k -= 1
+//         }
+//         if (redraw) {
+//           iterations = lvl - itemData.styles[l].lvl
+//           k = itemData.transformers.length - 1
+//           while (iterations > 0) {
+//             mat.multiply(itemData?.transformers[k].mProps.v)
+//             iterations -= 1
+//             k -= 1
+//           }
+//         }
+//       } else {
+//         mat = this._identityMatrix
+//       }
+//       paths = itemData.sh.paths
+//       jLen = paths._length
+//       if (redraw) {
+//         pathStringTransformed = ''
+//         for (j = 0; j < jLen; j += 1) {
+//           pathNodes = paths.shapes[j]
+//           if (pathNodes && pathNodes._length) {
+//             pathStringTransformed += buildShapeString(
+//               pathNodes as unknown as ShapeData,
+//               pathNodes._length,
+//               pathNodes.c,
+//               mat
+//             )
+//           }
+//         }
+//         itemData.caches[l] = pathStringTransformed
+//       } else {
+//         pathStringTransformed = itemData.caches[l]
+//       }
+//       itemData.styles[l].d += styleData.hd === true ? '' : pathStringTransformed
+//       itemData.styles[l]._mdf = redraw || itemData.styles[l]._mdf
+//     }
+//   }
+
+//   private static renderStroke(
+//     _: any,
+//     itemData: ItemData,
+//     isFirstFrame: boolean
+//   ) {
+//     const styleElem = itemData.style
+//     const d = itemData.d
+//     if (d && (d._mdf || isFirstFrame) && d.dashStr) {
+//       styleElem.pElem.setAttribute('stroke-dasharray', d.dashStr)
+//       styleElem.pElem.setAttribute('stroke-dashoffset', `${d.dashoffset[0]}`)
+//     }
+//     if (itemData.c && (itemData.c._mdf || isFirstFrame)) {
+//       styleElem.pElem.setAttribute(
+//         'stroke',
+//         `rgb(${Math.floor((itemData.c.v as number[])[0])},${Math.floor((itemData.c.v as number[])[1])},${(itemData.c.v as number[])[2]})`
+//       )
+//     }
+//     if (itemData.o._mdf || isFirstFrame) {
+//       styleElem.pElem.setAttribute('stroke-opacity', itemData.o.v as string)
+//     }
+//     if (itemData.w._mdf || isFirstFrame) {
+//       styleElem.pElem.setAttribute('stroke-width', itemData.w.v as string)
+//       if (styleElem.msElem) {
+//         styleElem.msElem.setAttribute('stroke-width', itemData.w.v as string)
+//       }
+//     }
+//   }
+// }
--- src/renderers/SVGRenderer.ts
+++ src/renderers/SVGRenderer.ts
@@ -5,96 +5,96 @@ import SVGCompElement from '@/elements/svg/SVGCompElement'
 import { RendererType } from '@/enums'
 import SVGRendererBase from '@/renderers/SVGRendererBase'
 import { createNS } from '@/utils'
-import { extendPrototype } from '@/utils/functionExtensions'
+// import { applyMixins } from '@/utils/functionExtensions'
 import { createElementID } from '@/utils/getterSetter'
 
-/**
- *
- */
-export default function SVGRenderer(
-  this: {
-    animationItem: AnimationItem
-    layers: null | LottieLayer[]
-    renderedFrame: number
-    svgElement: SVGSVGElement
-    layerElement: SVGGElement
-    renderConfig: SVGRendererConfig
-    globalData: GlobalData
-    elements: any[]
-    pendingElements: any[]
-    destroyed: boolean
-    rendererType: RendererType
-  },
-  animationItem: AnimationItem,
-  config?: SVGRendererConfig
-) {
-  this.animationItem = animationItem
-  this.layers = null
-  this.renderedFrame = -1
-  this.svgElement = createNS('svg')
-  let ariaLabel = ''
-  if (config?.title) {
-    const titleElement = createNS('title')
-    const titleId = createElementID()
-    titleElement.setAttribute('id', titleId)
-    titleElement.textContent = config.title
-    this.svgElement.appendChild(titleElement)
-    ariaLabel += titleId
-  }
-  if (config?.description) {
-    const descElement = createNS('desc')
-    const descId = createElementID()
-    descElement.setAttribute('id', descId)
-    descElement.textContent = config.description
-    this.svgElement.appendChild(descElement)
-    ariaLabel += ` ${descId}`
-  }
-  if (ariaLabel) {
-    this.svgElement.setAttribute('aria-labelledby', ariaLabel)
-  }
-  const defs = createNS<SVGDefsElement>('defs')
-  this.svgElement.appendChild(defs)
-  const maskElement = createNS<SVGGElement>('g')
-  this.svgElement.appendChild(maskElement)
-  this.layerElement = maskElement
-  this.renderConfig = {
-    className: config?.className || '',
-    contentVisibility: config?.contentVisibility || 'visible',
-    filterSize: {
-      height: (config?.filterSize && config.filterSize.height) || '100%',
-      width: (config?.filterSize && config.filterSize.width) || '100%',
-      x: (config?.filterSize && config.filterSize.x) || '0%',
-      y: (config?.filterSize && config.filterSize.y) || '0%',
-    },
-    focusable: config?.focusable,
-    height: config?.height,
-    hideOnTransparent: !(config?.hideOnTransparent === false),
-    id: config?.id || '',
-    imagePreserveAspectRatio:
-      config?.imagePreserveAspectRatio || 'xMidYMid slice',
-    preserveAspectRatio: config?.preserveAspectRatio || 'xMidYMid meet',
-    progressiveLoad: config?.progressiveLoad || false,
-    runExpressions:
-      !config || config.runExpressions === undefined || config.runExpressions,
-    viewBoxOnly: config?.viewBoxOnly || false,
-    viewBoxSize: config?.viewBoxSize || false,
-    width: config?.width,
+export default class SVGRenderer extends SVGRendererBase {
+  animationItem: AnimationItem
+  destroyed: boolean
+  elements: any[]
+  globalData: null | GlobalData
+  layerElement: SVGGElement
+  layers: null | LottieLayer[]
+  pendingElements: any[]
+  renderConfig: SVGRendererConfig
+  renderedFrame: number
+  rendererType: RendererType
+  svgElement: SVGSVGElement
+  constructor(animationItem: AnimationItem, config?: SVGRendererConfig) {
+    super()
+    this.animationItem = animationItem
+    this.layers = null
+    this.renderedFrame = -1
+    this.svgElement = createNS('svg')
+    let ariaLabel = ''
+    if (config?.title) {
+      const titleElement = createNS('title')
+      const titleId = createElementID()
+      titleElement.setAttribute('id', titleId)
+      titleElement.textContent = config.title
+      this.svgElement.appendChild(titleElement)
+      ariaLabel += titleId
+    }
+    if (config?.description) {
+      const descElement = createNS('desc')
+      const descId = createElementID()
+      descElement.setAttribute('id', descId)
+      descElement.textContent = config.description
+      this.svgElement.appendChild(descElement)
+      ariaLabel += ` ${descId}`
+    }
+    if (ariaLabel) {
+      this.svgElement.setAttribute('aria-labelledby', ariaLabel)
+    }
+    const defs = createNS<SVGDefsElement>('defs')
+    this.svgElement.appendChild(defs)
+    const maskElement = createNS<SVGGElement>('g')
+    this.svgElement.appendChild(maskElement)
+    this.layerElement = maskElement
+    this.renderConfig = {
+      className: config?.className || '',
+      contentVisibility: config?.contentVisibility || 'visible',
+      filterSize: {
+        height: (config?.filterSize && config.filterSize.height) || '100%',
+        width: (config?.filterSize && config.filterSize.width) || '100%',
+        x: (config?.filterSize && config.filterSize.x) || '0%',
+        y: (config?.filterSize && config.filterSize.y) || '0%',
+      },
+      focusable: config?.focusable,
+      height: config?.height,
+      hideOnTransparent: !(config?.hideOnTransparent === false),
+      id: config?.id || '',
+      imagePreserveAspectRatio:
+        config?.imagePreserveAspectRatio || 'xMidYMid slice',
+      preserveAspectRatio: config?.preserveAspectRatio || 'xMidYMid meet',
+      progressiveLoad: config?.progressiveLoad || false,
+      runExpressions:
+        !config || config.runExpressions === undefined || config.runExpressions,
+      viewBoxOnly: config?.viewBoxOnly || false,
+      viewBoxSize: config?.viewBoxSize || false,
+      width: config?.width,
+    }
+
+    this.globalData = {
+      _mdf: false,
+      defs: defs,
+      frameNum: -1,
+      frameRate: 60,
+      renderConfig: this.renderConfig,
+    }
+    this.elements = []
+    this.pendingElements = []
+    this.destroyed = false
+    this.rendererType = RendererType.SVG
   }
 
-  this.globalData = {
-    _mdf: false,
-    defs: defs,
-    frameNum: -1,
-    renderConfig: this.renderConfig,
+  createComp(data: LottieLayer) {
+    return new SVGCompElement(data, this.globalData, this as any)
   }
-  this.elements = []
-  this.pendingElements = []
-  this.destroyed = false
-  this.rendererType = RendererType.SVG
 }
 
-extendPrototype([SVGRendererBase], SVGRenderer)
+// applyMixins(SVGRenderer, [SVGRendererBase])
 
-SVGRenderer.prototype.createComp = function (data: LottieLayer) {
-  return new (SVGCompElement as any)(data, this.globalData, this)
-}
+// interface SVGRenderer extends SVGRendererBase {}
+
+// export default SVGRenderer
